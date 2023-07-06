c50293e6545f3028de984a1b300ab7ea4d77a5fe
baechanyong1 <110149885+baechanyong1@users.noreply.github.com>
2023-07-06 15:37:57 +0900

    필요없는 주석제거

--- routes/like.js
+++ routes/like.js
@@ -4,46 +4,6 @@ const { Posts } = require("../models");
 // Middleware
 const authMiddleware = require("../middleware/authMiddleware");
 
-// router.post("/like/:postId", async (req, res) => {
-//   const postId = req.params.postId;
-
-//   try {
-//     // Post 모델에서 해당 postId의 게시글 조회
-//     const post = await Posts.findOne(postId);
-
-//     if (!post) {
-//       // 게시글이 없는 경우
-//       return res
-//         .status(404)
-//         .json({ success: false, message: "게시글을 찾을 수 없습니다." });
-//     }
-
-//     if (post.likeCount > 0) {
-//       // 좋아요 삭제
-//       post.likeCount -= 1;
-//       await post.save();
-//       return res.json({
-//         success: true,
-//         message: "좋아요 삭제",
-//         likeCount: post.likeCount,
-//       });
-//     } else {
-//       // 좋아요 생성
-//       post.likeCount = 1;
-//       await post.save();
-//       return res.json({
-//         success: true,
-//         message: "좋아요 생성",
-//         likeCount: post.likeCount,
-//       });
-//     }
-//   } catch (error) {
-//     console.error("오류 발생", error);
-//     return res
-//       .status(500)
-//       .json({ success: false, message: "서버 오류가 발생했습니다." });
-//   }
-// });
 
 router.put("/like/:postId", authMiddleware, async (req, res) => {
   const postId = req.params.postId;
